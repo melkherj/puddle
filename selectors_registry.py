43adcd0fe761182a51d8c09e69f9ac294a8c305b
Matthew Elkherj <melkherj@gmail.com>
2016-05-08 15:08:58 -0700

    registered entropy selector.  properly nuked active_conf from git

--- selectors_registry.py
+++ selectors_registry.py
@@ -1,7 +1,9 @@
 from selectors.random_selector import RandomSelector
+from selectors.entropy_selector import EntropySelector
 
 #@author melkherj
 
 selectors_registry = {
     'random':RandomSelector,
+    'entropy':EntropySelector,
 }
