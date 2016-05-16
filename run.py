50eac5edb8784cbc35c32ce1d3fed2413310858e
Matthew Elkherj <melkherj@gmail.com>
2016-05-15 18:07:56 -0700

    now running a the adaptive thresholding variant with a range of thresholds

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import random
 conf = load_conf()
 for i in range(1000):
     conf['random_seed'] = i
+    conf['selector']['active_thresh'] = 0.4*random.random()+0.3 #0.3->0.7
     results = active_evaluate(conf)
     append_results(results)
 
