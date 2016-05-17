a25095a49d4f68fc20c7daa8516f17a05fb5e340
Matthew Elkherj <melkherj@gmail.com>
2016-05-17 16:07:07 -0700

    run a bunch of variants of experiments

--- run.py
+++ run.py
@@ -5,11 +5,10 @@ import random
 # run some selector, see what performance is like, append to the results file
 conf = load_conf()
 conf['random_seed'] = 0
-#for semisup_selector in ['random','top']
-#for active_selector in ['adaptive','random']:
-#    conf['active_selector']['name'] = active_selector
-    
-for i in range(1000):
-    results = active_evaluate(conf)
-    append_results(results)
+for semisup_selector in ['random','empty','top']:
+    conf['semisup_selector']['name'] = semisup_selector
+    for active_selector in ['adaptive','random']:
+        conf['active_selector']['name'] = active_selector
+        results = active_evaluate(conf)
+        append_results(results)
 
